ce534e542506f812fb533d0a13dbf8c4bb2aa99f
nmancus1 <nmancus1@dtcc.edu>
2017-11-09 16:50:18 -0500

    added try-catch to avoid input errors, added additional input parameter

--- src/moderntempcalc/CalculateTemp.java
+++ src/moderntempcalc/CalculateTemp.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package moderntempcalc;
 
-/**
- *
- * @author nick
- */
 public class CalculateTemp {
     
     private static String result = "";
@@ -17,14 +9,14 @@ public class CalculateTemp {
     
     public static void setResult (String userInput, char userTempType) {        //  Method to set result value, called from WelcomeScene
         
-        Double userTemp;                                                        //  Variable to store parsed user input
+        Double userTempInput;                                                   //  Variable to store parsed user input
         char resultTempType = 'C';                                              //  Variable to store the temp type of result
         double resultUserTemp = 0.0;                                            //  Variable to store resulting temperature
         
     
     //  Try - catch to grab incorrect user input
         try {
-                userTemp = Double.parseDouble(userInput);                       //  Parses user input to a double
+                userTempInput = Double.parseDouble(userInput);                  //  Parses user input to a double
             }
          catch (java.lang.NumberFormatException e) {                            //  If the user enters something that is not an integer
                result = "Please enter numbers only.";
@@ -35,15 +27,15 @@ public class CalculateTemp {
     //  Conversion algorithm below
         switch (userTempType) {
             
-            case 'C':   resultUserTemp = (userTemp-32)* 5/9;                    // Convert C to F
+            case 'C':   resultUserTemp = (userTempInput-32)* 5/9;               // Convert C to F
                         break;
                         
-            case 'F':   resultUserTemp = userTemp * 9/5 +32;                    // Convert F to C
+            case 'F':   resultUserTemp = userTempInput * 9/5 +32;               // Convert F to C
                         break;     
         }
         
     //  Round result and input to two decimal places, for cleaner output
-        userTemp = (double) Math.round(userTemp * 100) / 100;
+        userTempInput = (double) Math.round(userTempInput * 100) / 100;
         resultUserTemp = (double) Math.round(resultUserTemp * 100) / 100;
 
         
@@ -57,7 +49,7 @@ public class CalculateTemp {
         
     
     //  Convert back to string and assign to result variable
-        result = (userTemp + "°" + resultTempType + " equals " + resultUserTemp + "°" + userTempType);
+        result = (userTempInput + "°" + resultTempType + " equals " + resultUserTemp + "°" + userTempType);
         }
     
     
